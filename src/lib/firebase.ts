a1f7e71a120ffdc1d355205e392200c127961226
hiroki_saito <longest3110@gmail.com>
2026-03-01 01:16:19 +0900

    不要な変数削除

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,9 +6,6 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase (singleton pattern)

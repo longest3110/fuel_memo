4bc006c1dee3256e0afabdc42908cb55d86e70b1
hiroki_saito <longest3110@gmail.com>
2026-03-01 14:20:49 +0900

    ビルド設定追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/fuel_memo',
+  assetPrefix: '/fuel_memo/',
 };
 
 export default nextConfig;

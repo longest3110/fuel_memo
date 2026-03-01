4ed89ba76f5c4514240fb661c279cb0e8ae80873
hiroki_saito <longest3110@gmail.com>
2026-03-01 15:03:37 +0900

    basepath調整

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next';
 
+const isExport = process.env.NODE_ENV === 'production';
+const base_path = '/fuel_memo';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/fuel_memo',
-  assetPrefix: '/fuel_memo/',
+  basePath: isExport ? base_path : '',
+  assetPrefix: isExport ? base_path + '/' : undefined,
 };
 
 export default nextConfig;
